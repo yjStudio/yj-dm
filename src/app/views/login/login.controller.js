0c07106d4a15db0bd0bef2bad1b4731baf9a1064
tmirun <tmirun@hotmail.com>
2016-08-06 14:17:23 +0200

    login page add firebase call

--- src/app/views/login/login.controller.js
+++ src/app/views/login/login.controller.js
@@ -6,7 +6,24 @@
     .controller('LoginController', LoginController);
 
   /** @ngInject */
-  function LoginController(){
+  function LoginController($log, STORE_user, toastr){
+
     var vm = this;
+    vm.email = "yjstudio.2016@gmail.com";
+    vm.password;
+
+    vm.login = function(){
+      firebase.auth().signInWithEmailAndPassword(vm.email, vm.password)
+      .then(function(user){
+        $log.debug("Login ok", user);
+        toastr.success("登入成功");
+      })
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        $log.error("Login error", errorCode, errorMessage);
+        toastr.error(errorMessage, "登入失败");
+      });
+    }
   }
 })();
