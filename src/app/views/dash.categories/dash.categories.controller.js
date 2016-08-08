a2a06df0a443222fa9702a332a992805a5294b01
tmirun <tmirun@hotmail.com>
2016-08-08 13:29:47 +0200

    buiding categories view

--- src/app/views/dash.categories/dash.categories.controller.js
+++ src/app/views/dash.categories/dash.categories.controller.js
@@ -6,15 +6,27 @@
     .controller('DashCategoriesController', DashCategoriesController);
 
   /** @ngInject */
-  function DashCategoriesController($log, $state, STORE_categories){
+  function DashCategoriesController($log, $state, $mdDialog, STORE_categories){
 
     $log.debug("Init categories controller");
 
     var vm = this;
+    vm.categories;
 
-    // STORE_categories.getCategories()
-    // .then(function(data){
-    //   console.log(data);
-    // });
+    STORE_categories.getCategories()
+    .then(function(data){
+      vm.categories = STORE_categories.categories;
+    })
+
+    vm.showDialogCategory = function(category){
+      $mdDialog.show({
+        controller: "DiaCategoryController as vm",
+        templateUrl: 'app/components/dia.category/dia.category.html',
+        locals:{
+          category: category
+        },
+        clickOutsideToClose:true,
+      })
+    }
   }
 })();
