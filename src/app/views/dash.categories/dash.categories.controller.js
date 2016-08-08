9adda3d9d8915fc9b0f47c6d57e82002d64415f8
tmirun <tmirun@hotmail.com>
2016-08-08 15:37:42 +0200

    buiding dash.categories

--- src/app/views/dash.categories/dash.categories.controller.js
+++ src/app/views/dash.categories/dash.categories.controller.js
@@ -18,15 +18,31 @@
       vm.categories = STORE_categories.categories;
     })
 
-    vm.showDialogCategory = function(category){
+    STORE_categories.event.on("change", function(){
+      vm.categories = STORE_categories.categories;
+    })
+
+    vm.showDialogCategory = function(categoryId){
       $mdDialog.show({
         controller: "DiaCategoryController as vm",
         templateUrl: 'app/components/dia.category/dia.category.html',
         locals:{
-          category: category
+          categoryId: categoryId
         },
         clickOutsideToClose:true,
       })
+    };
+
+    vm.remove = function(key, name){
+      // Appending dialog to document.body to cover sidenav in docs app
+      var confirm = $mdDialog.confirm()
+            .title('确定你要删除 "'+name+'" 这个分类吗')
+            .textContent("一旦删除将无法找回")
+            .ok('删除')
+            .cancel('关闭');
+      $mdDialog.show(confirm).then(function() {
+        STORE_categories.removeCategory(key);
+      });
     }
   }
 })();
