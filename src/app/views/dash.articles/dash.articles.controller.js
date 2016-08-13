13516f5c484d22f3ea2d529f6a6907585b16832b
tmirun <tmirun@hotmail.com>
2016-08-13 13:18:20 +0200

    show articles to data table

--- src/app/views/dash.articles/dash.articles.controller.js
+++ src/app/views/dash.articles/dash.articles.controller.js
@@ -6,26 +6,43 @@
     .controller('DashArticlesController', DashArticlesController);
 
   /** @ngInject */
-  function DashArticlesController($log, $state, $mdDialog, Upload, STORE_articles){
+  function DashArticlesController($log, $scope, $state, $mdDialog, Util, Upload, STORE_articles){
 
     $log.debug("Init categories controller");
 
     var vm = this;
+    vm.selected = [];
+    vm.articles = STORE_articles.articles;
+
+    vm.promise = STORE_articles.getArticles();
+
+    STORE_articles.event.on("change", function(){
+      vm.articles = STORE_articles.articles
+    });
 
     vm.showDialogArticle = function(articleId){
       $mdDialog.show({
         controller: "DiaArticleController as vm",
         templateUrl: 'app/components/dia.article/dia.article.html',
         locals:{
-          articleId: 'fsfdsafdasafsd'
+          articleId: articleId
         },
-        clickOutsideToClose:true,
+        clickOutsideToClose:true
+      })
+    };
+
+    vm.showDialogArticleCollective = function(){
+      $mdDialog.show({
+        controller: "DiaArticleCollectiveController as vm",
+        templateUrl: 'app/components/dia.articleCollective/dia.articleCollective.html',
+        clickOutsideToClose:true
       })
     };
 
     // for multiple files:
-    vm.uploadFiles = function (files, error) {
+    vm.uploadFiles = function (files) {
       var file;
+      $log.debug("upload images", files);
       if (files && files.length) {
         for (var i = 0; i < files.length; i++) {
           file = files[i];
@@ -42,5 +59,6 @@
         });
       })
     }
+
   }
 })();
