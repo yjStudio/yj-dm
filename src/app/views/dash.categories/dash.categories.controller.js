ca232e370d7753b2439d12901c8fe51d5eb7c192
tmirun <tmirun@hotmail.com>
2016-08-21 11:18:05 +0200

    building dashboard

--- src/app/views/dash.categories/dash.categories.controller.js
+++ src/app/views/dash.categories/dash.categories.controller.js
@@ -12,10 +12,37 @@
 
     var vm = this;
     vm.categories;
+    vm.categoriesMenuItems;
+
+    vm.tree2 = [{
+      'id': 1,
+      'name': 'tree2 - item1',
+      'nodes': []
+    }, {
+      'id': 2,
+      'name': 'tree2 - item2',
+      'nodes': []
+    }, {
+      'id': 3,
+      'name': 'tree2 - item3',
+      'nodes': []
+    }, {
+      'id': 4,
+      'name': 'tree2 - item4',
+      'nodes': []
+    }];
+
+    vm.categoriesMenu = [];
 
     STORE_categories.getCategories()
     .then(function(){
       vm.categories = STORE_categories.categories;
+      var tmp = angular.copy(vm.categories);
+      vm.categoriesMenuItems = Object.keys(tmp).map(function(key) {
+        var object = tmp[key]
+        object.categories = [];
+        return object;
+      });
     })
 
     STORE_categories.event.on("change", function(){
@@ -36,7 +63,7 @@
     vm.remove = function(key, name){
       // Appending dialog to document.body to cover sidenav in docs app
       var confirm = $mdDialog.confirm()
-            .title('确定你要删除 "'+name+'" 这个分类吗')
+            .name('确定你要删除 "'+name+'" 这个分类吗')
             .textContent("一旦删除将无法找回")
             .ok('删除')
             .cancel('关闭');
