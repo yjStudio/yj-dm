7aba972413007f9c80aa33cbb6ae245b3b9e8890
tmirun <tmirun@hotmail.com>
2016-08-25 23:22:03 +0200

    remove ngTouch module

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -4,7 +4,6 @@
   angular
     .module('yjDm', ['ngAnimate',
                      'ngCookies',
-                     'ngTouch',
                      'ngSanitize',
                      'ngMessages',
                      'ngAria',
